335c2ea2038c5836e5f5fc496dbdb57581258973
VuqarBaxisov <baxisovvuqar97@gmail.com>
2023-12-24 21:45:55 +0400

    first commit

---
+++ FirstLessonPrograming/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first number");
+        int var1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter second number");
+
+        int var2 = int.Parse(Console.ReadLine());
+
+        int sum = var1 + var2;
+
+        Console.WriteLine("Sum is" + " " + sum);
+
+
+    }
+
+}
